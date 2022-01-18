c3c2e4d15d80667318fae3f73767e196bd1956e8
SreevidyaAlivelu0106 <sxa200150@utdallas.edu>
2022-01-18 11:53:04 -0600

    commit no 10

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ module.exports=async()=>{
       useUnifiedTopology: true
     };
     await mongoose.connect(
-      "mongodb+srv://sreevidyadev:Sree1234@cluster0.qy684.mongodb.net/todo-app?retryWrites=true&w=majority",
+      process.env.MONGODB_CONNECTION_STRING,
 
       connectionParams
     );
