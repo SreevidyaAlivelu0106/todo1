08de36a2e6eb4226c13de0567e71fdc33b291b24
SreevidyaAlivelu0106 <sxa200150@utdallas.edu>
2022-01-18 14:41:21 -0600

    commit no 31

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ module.exports=async()=>{
   try{
 
     require("dotenv").config()
-     mongoose.connect(
+      mongoose.connect(
       process.env.MONGO_URI,
              {
                useNewUrlParser: true,
--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const dotenv = require("dotenv");
+const mongoose = require('mongoose');
 
 dotenv.config();
 
 const cors=require('cors');
 const express=require('express');
-const connection=require('./db');
+//const connection=require('./db');
 const tasks=require("./routes/tasks")
 const app=express();
-connection();
+//connection();
 app.use(express.json());
 app.use(cors());
 
+mongoose.connect(
+process.env.MONGO_URI,
+       {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+       }
+);
+console.log("Connected to Data Base");
+}
+catch(error){
+console.log("Could not connect to Data Base", error);
+}
+
 
 app.use("/api/tasks", tasks);
-
 
 // Accessing the path module
 const path = require("path");
