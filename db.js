698f52c9cc195869e77a94865b87322eccc491f0
SreevidyaAlivelu0106 <sxa200150@utdallas.edu>
2022-01-18 14:32:25 -0600

    commit no 30

--- db.js
+++ db.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose');
 module.exports=async()=>{
   try{
-    const connectionParams={
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    };
 
     require("dotenv").config()
-    await mongoose.connect(
-      process.env.MONGODB_CONNECTION_STRING,
-
-      connectionParams
+     mongoose.connect(
+      process.env.MONGO_URI,
+             {
+               useNewUrlParser: true,
+               useUnifiedTopology: true,
+             }
     );
     console.log("Connected to Data Base");
   }
