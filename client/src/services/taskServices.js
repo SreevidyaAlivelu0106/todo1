c4ac2bb4d607ccf488a3447b1f0d26004abd5d97
SreevidyaAlivelu0106 <sxa200150@utdallas.edu>
2022-01-18 13:40:03 -0600

    commit no 27

--- client/src/services/taskServices.js
+++ client/src/services/taskServices.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const apiUrl= "https://secret-castle-57709.herokuapp.com/ap/tasks";
+const apiUrl= "https://secret-castle-57709.herokuapp.com/api/tasks";
 
 export function getTasks(){
   return axios.get(apiUrl);
