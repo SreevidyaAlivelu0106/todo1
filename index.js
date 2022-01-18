26cacbfc20b39e3d3d36383633145ebe41c88656
SreevidyaAlivelu0106 <sxa200150@utdallas.edu>
2022-01-18 10:09:58 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const cors=require('cors');
+const express=require('express');
+const connection=require('./db');
+const tasks=require("./routes/tasks")
+const app=express();
+connection();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/tasks", tasks);
+
+const port=process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
