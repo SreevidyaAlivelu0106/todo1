7e26164b447da3b127ba390e8f2d6bff12a87129
SreevidyaAlivelu0106 <sxa200150@utdallas.edu>
2022-01-18 14:48:47 -0600

    commit no 33

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ const app=express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(
+try{mongoose.connect(
 process.env.MONGO_URI,
        {
          useNewUrlParser: true,
          useUnifiedTopology: true,
        }
 );
-console.log("Connected to Data Base");
+console.log("Connected to Data Base");}
 
 catch(error){
 console.log("Could not connect to Data Base", error);
