17b19f4f4f2b9e3186230caf40ba82a99f2a583c
SreevidyaAlivelu0106 <sxa200150@utdallas.edu>
2022-01-18 10:47:54 -0600

    commit no 4

--- index.js
+++ index.js
@@ -10,4 +10,17 @@ app.use(cors());
 app.use("/api/tasks", tasks);
 
 const port=process.env.PORT || 8080;
+
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+
 app.listen(port, () => console.log(`Listening on port ${port}...`));
